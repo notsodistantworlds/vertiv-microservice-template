de957b53a7400b3e2705494533b12cb5c5bdf9ec
Bohdan Strutynskyi <strutinsky5386@gmail.com>
2020-06-19 01:44:03 +0300

    Make it run

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Vert.x groovy template microservice
 
 Boilerplate to get things going
+
+### Build
+
+``mvn install``
+
+### Run
+
+``java -jar ./target/project-name-version-jar-with-dependencies.jar``
--- src/main/groovy/nsdw/template/TemplateTypicalServerVerticle.groovy
+++ src/main/groovy/nsdw/template/TemplateTypicalServerVerticle.groovy
@@ -1,7 +1,17 @@
 package nsdw.template
 
+import io.vertx.core.DeploymentOptions
+import io.vertx.core.Vertx
+import io.vertx.core.json.JsonObject
 import nsdw.vertiv.core.TypicalServerVerticle
 
 class TemplateTypicalServerVerticle extends TypicalServerVerticle {
    // ...
+}
+
+class TemplateTypicalServerVerticleStarter {
+    public static void main(String[] args) {
+        def opts = new DeploymentOptions().setConfig(new JsonObject().put("http", new JsonObject().put("port", 53863)))
+        Vertx.vertx().deployVerticle(new TypicalServerVerticle(), opts)
+    }
 }
